2ff4668aaf670d978f0bc7915c573d0481f93a78
Ori Saporta <ori@vfunction.com>
2020-10-16 17:16:59 +0300

    Added first draft at sets

---
+++ set_solver.py
@@ -0,0 +1,246 @@
+# requires pysmt.  To get it, do:
+# `pip install pysmt`
+# `pysmt-install --z3`
+# u can consult `https://github.com/pysmt/pysmt` for more info.
+# let me know if u need help with that
+
+# import stuff from pysmt
+from pysmt.shortcuts import *
+from datetime import datetime
+from itertools import product
+import os
+
+####################
+# helper functions #
+####################
+n = None
+k = None
+l = None
+q = None
+ops = None
+
+def le(x, y):
+    """x<=y = /\{i,j} x_ij => y_ij"""
+    return And([Implies(x[(i, j)], y[(i, j)]) for i, j in product(range(len(ops)), range(len(ops)))])
+
+
+def lt(x, y):
+    """x<y = x<=y /\ \/_{ij} ~x_ij /\ y_ij"""
+    return And(le(x, y), Or([And(Not(x[(i, j)]), y[(i, j)]) for i, j in product(range(len(ops)), range(len(ops)))]))
+
+
+def irr(y):
+    """irr(y) = ~y00 /\ ~y11 /\ .."""
+    return And([Not(y[(i, i)]) for i in range(len(ops))])
+
+
+def trans(y):
+    """trans(y) = /\_{i,j,k} y_ij /\ y_jk => y_ik"""
+    return And([Implies(And(y[(i, j)], y[(j, k)]), y[(i, k)]) for i, j, k in product(range(len(ops)), range(len(ops)), range(len(ops))) if (i != j and j != k)])
+
+
+def po(y):
+    """po(y): irr + trans"""
+    return And(irr(y), trans(y))
+
+
+def tot(y):
+    """tot(y) = /\_{i != j} y_ij \/ y_ji"""
+    return And([Or(y[(i, j)], y[(j, i)]) for i, j in product(range(len(ops)), range(len(ops))) if i != j])
+
+def gen_conditions(y, n, k, j):
+    """ 
+    for each pair i,j s.t. i := add(v) and j := remove(v) add y[(i,j)]
+    """
+    if n < 1:
+        raise Exception("n must be at least 1")
+    group_size = 2 + k + l
+    conditions = []
+    for i in range(n):
+        group_index = i*(group_size)
+        # add before remove
+        conditions.append(y[(group_index, group_index+1)])
+        for j in range(k):
+            # in_true after add
+            conditions.append(y[group_index, group_index+2+j])
+            # in_true before remove
+            conditions.append(y[group_index+2+j, group_index+1])
+        for j in range(l):
+            # if in_false after add it's after remove
+            conditions.append(
+                Iff(y[(group_index, group_index+2+k+j)], y[(group_index+1, group_index+2+k+j)]))
+            # if in_false before add it's before remove
+            conditions.append(
+                Iff(y[(group_index+2+k+j, group_index)], y[(group_index+2+k+j, group_index+1)]))
+    return conditions
+
+
+def set_O(y, n, k, l):
+    return And(gen_conditions(y, n, k, l))
+
+
+def testcase(x):
+    """testcase(x): po(x) /\ (forall y, x <=y /\ po(y) /\ tot(y) => ~set(O,y))"""
+    global ops, n, k, l
+    y = {(i, j): Symbol("y_" + label(i) + "_" + label(j))
+         for i, j in product(range(len(ops)), range(len(ops)))}
+    return And(po(x), ForAll([y[(i, j)] for i, j in product(range(len(ops)), range(len(ops)))], Implies(And(le(x, y), po(y), tot(y)), Not(set_O(y, n, k, l)))))
+
+
+def gen_ops(n, k, l):
+    result = []
+    for i in range(n):
+        result.append("add(" + chr(65 + int(i)) + ")")
+        result.append("remove(" + chr(65 + int(i)) + ")")
+        for j in range(k):
+            result.append("in(" + chr(65 + int(i)) + ")-t-" + str(j))
+        for j in range(l):
+            result.append("in(" + chr(65 + int(i)) + ")-f-" + str(j))
+    return result
+
+
+def label(i):
+    global ops
+    return ops[i]
+
+
+def minimal_one(rel, i0, j0):
+    global ops
+    z = {(i, j): rel[(i, j)]
+         for i, j in product(range(len(ops)), range(len(ops)))}
+    z[(i0, j0)] = Not(rel[(i0, j0)])
+    return Not(testcase(z))
+
+
+def minimal_all(rel):
+    global ops
+    return And([Implies(rel[(i, j)], minimal_one(rel, i, j)) for i, j in product(range(len(ops)), range(len(ops)))])
+
+
+def gi(x, e1, rel):
+    terms = And(gi_t1(x), gi_t2(x), gi_t3(x, e1, rel), gi_t4(x))
+    return terms
+
+
+def gi_strong(x, e1, rel):
+    return And(gi(x, e1, rel), gi_t5(x))
+
+
+def gi_t1(x):
+    global ops
+    return And([Or([x[i, j] for j in range(len(ops))]) for i in range(len(ops))])
+
+
+def gi_t2(x):
+    global ops
+    return And([Or(Not(x[i, k]), Not(x[j, k])) for i, j, k in product(range(len(ops)), range(len(ops)), range(len(ops))) if i != j])
+
+
+def gi_t3(x, e1, rel):
+    """ x[i,k] && x[j,l] --> (e1[i,j] <--> rel[k,l]) """
+    global ops
+    return And([Implies(Iff(e1[i, j], Not(rel[k, l])), Or(Not(x[i, k]), Not(x[j, l]))) for i, j, k, l in product(range(len(ops)), range(len(ops)), range(len(ops)), range(len(ops))) if l != k and i != j])
+
+
+def gi_t4(x):
+    """
+    "Color coding": mapped ops should be of the same type (just different value)
+    """
+    global ops
+    return And([Not(x[i, j]) for i, j in product(range(len(ops)), range(len(ops))) if opType(i) != opType(j)])
+
+
+def gi_t5(x):
+    """remap by values"""
+    global ops
+    return And([Implies(x[i, j], x[i+m, j+m]) for i, j in product(range(len(ops)), range(len(ops))) for m in range(1, 2+k+l) if opType(i) == 0 and opType(j) == 0])
+
+
+def opType(i):
+    s = ops[i]
+    desc = s[:s.index("(")]+s[s.index(")")+1:]
+    if desc == "add":
+        return 0
+    elif desc == "remove":
+        return 1
+    elif desc == "in-t":
+        return 2
+    elif desc == "in-f":
+        return 3
+
+def main():
+    # set type we're working with: ?
+    global q
+    q = "set"
+
+    # number of values
+    global n
+    n = 2
+
+    # number of in per value that return true:
+    global k
+    k = 1
+
+    # number of in per value that return false:
+    global l
+    l = 1
+
+    global ops
+    ops = gen_ops(n, k, l)
+
+    print("start", datetime.now())
+    print("n:", n, "k:", k, "l:", l)
+
+    # path = os.path.join("tests","generated", q+"_"+str(n)+"_"+str(k))
+    # if not os.path.exists(path):
+    #     os.mkdir(path)
+
+    try:
+        # create a solver
+        solver = Solver("z3")
+
+        # create Boolean variables
+        rel = {(i, j): Symbol(label(i)+"_"+label(j))
+               for i, j in product(range(len(ops)), range(len(ops)))}
+
+        x = {(i, j): Symbol("x_"+label(i)+"_"+label(j))
+             for i, j in product(range(len(ops)), range(len(ops)))}
+
+        # assert
+        solver.add_assertion(testcase(rel))
+        solver.add_assertion(minimal_all(rel))
+
+        # check satisfiability
+        res = solver.check_sat()
+
+        # get all possible models
+        i = 1
+        while (res):
+            # variables that are true in the model
+            model_pos = [rel[(i, j)] for i, j in product(
+                range(len(ops)), range(len(ops))) if solver.get_value(rel[(i, j)]).is_true()]
+
+            model = {(i, j): solver.get_value(rel[(i, j)])
+                     for i, j in product(range(len(ops)), range(len(ops)))}
+
+            print(i, ":", model_pos)
+            # test_data = generate_test(model_pos, n, k, q)
+            # f = open(os.path.join(path, "t"+str(i)+".c"), "w")
+            # f.write(test_data)
+            # f.close()
+            i += 1
+
+            # negate the variables that are true in the model
+            # assert positively the variables that are false in the model
+            # put everything in one disjunction
+
+            solver.add_assertion(Not(Exists(x.values(), gi(x, model, rel))))
+
+            res = solver.check_sat()
+
+    finally:
+        print("end", datetime.now())
+
+
+if __name__ == "__main__":
+    main()

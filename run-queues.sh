67e6ea62e971e274ec8cbb3d4fbe5d3545155be4
Ori Saporta <ori@vfunction.com>
2021-01-19 10:13:04 +0200

    add date printing to run script

--- run-queues.sh
+++ run-queues.sh
@@ -2,7 +2,7 @@
 
 echo "running generated tests for " $1
 
-for t in ./tests/generated/generic_10_0_10_True_4/*.c; do echo $t; timeout 3600 ../genmc/src/genmc -disable-race-detection -- -D$1 $t; done
-for t in ./tests/generated/generic_10_2_10_True_4/*.c; do echo $t; timeout 3600 ../genmc/src/genmc -disable-race-detection -- -D$1 $t; done
-for t in ./tests/generated/generic_10_0_10_True_5/*.c; do echo $t; timeout 3600 ../genmc/src/genmc -disable-race-detection -- -D$1 $t; done
-for t in ./tests/generated/generic_10_2_10_True_5/*.c; do echo $t; timeout 3600 ../genmc/src/genmc -disable-race-detection -- -D$1 $t; done
+for t in ./tests/generated/generic_10_0_10_True_4/*.c; do echo $t; date; timeout 3600 ../genmc/src/genmc -disable-race-detection -- -D$1 $t; done
+for t in ./tests/generated/generic_10_2_10_True_4/*.c; do echo $t; date; timeout 3600 ../genmc/src/genmc -disable-race-detection -- -D$1 $t; done
+for t in ./tests/generated/generic_10_0_10_True_5/*.c; do echo $t; date; timeout 3600 ../genmc/src/genmc -disable-race-detection -- -D$1 $t; done
+for t in ./tests/generated/generic_10_2_10_True_5/*.c; do echo $t; date; timeout 3600 ../genmc/src/genmc -disable-race-detection -- -D$1 $t; done

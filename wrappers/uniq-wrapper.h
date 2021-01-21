e09a3d3e17890330e980b9732ff6d0243d6bb933
Ori Saporta <ori@vfunction.com>
2021-01-21 15:22:08 +0200

    UNIQ - first optimization

--- wrappers/uniq-wrapper.h
+++ wrappers/uniq-wrapper.h
@@ -10,9 +10,11 @@
 
 #define MAX_SIZE 32
 
+typedef int val_t;
+
 typedef struct queue_t
 {
-    atomic_uint buffer[MAX_SIZE];
+    val_t* buffer[MAX_SIZE];
     int mask;
     atomic_int in, out;
     atomic_bool isUsed[MAX_SIZE];
@@ -27,48 +29,68 @@ void q_init_queue(queue_t *q, int num_threads)
     q->out = q->in = 0;
 }
 
-void q_enqueue(queue_t *q, unsigned int val)
+#define MAX_ITER 2
+
+void enqueue(queue_t *q, val_t * val)
 {
     int in, out;
+    bool used;
     // do
     // {
     //     i = q->in;
     //     if (i - q->out > q->mask)
     //         return;
     // } while (q->isUsed[i & q->mask] || !atomic_compare_exchange_weak(&(q->in), &i, i + 1));
-    in = atomic_load_explicit(&q->in, memory_order_relaxed);
-    out = atomic_load_explicit(&q->out, memory_order_relaxed);
-    if (in - out > q->mask)
-        return;
-    bool used = atomic_load_explicit(&q->isUsed[in & q->mask], memory_order_relaxed);
-    __VERIFIER_assume(!used);
-    __VERIFIER_assume(atomic_compare_exchange_weak_explicit(&(q->in), &in, in + 1, memory_order_relaxed, memory_order_relaxed));
+    int cnt = 0;
+    do 
+    {
+        __VERIFIER_assume(++cnt < MAX_ITER);
+        in = atomic_load_explicit(&q->in, memory_order_relaxed);
+        out = atomic_load_explicit(&q->out, memory_order_relaxed);
+        if (in - out > q->mask)
+            return;
+    } while (!atomic_compare_exchange_weak_explicit(&(q->in), &in, in + 1, memory_order_relaxed, memory_order_relaxed));
 
-    atomic_store_explicit(&q->buffer[in & q->mask], val, memory_order_relaxed);
-    atomic_store_explicit(&q->isUsed[in & q->mask], true, memory_order_relaxed);
+    q->buffer[in & q->mask] = val;
+    atomic_store_explicit(&q->isUsed[in & q->mask], true, memory_order_release);
     return;
 }
 
-bool q_dequeue(queue_t *q, unsigned int *retVal)
+void q_enqueue(queue_t *q, int val) {
+    val_t *v = (val_t*)malloc(sizeof(val_t));
+    *v = val;
+    enqueue(q, v);
+}
+
+bool dequeue(queue_t *q, val_t **retVal)
 {
     int out, in;
+    bool used;
     // do
     // {
     //     o = q->out;
     //     if (o == q->in)
     //         return -1;
     // } while (!q->isUsed[o & q->mask] || !atomic_compare_exchange_weak(&(q->out), &o, o + 1));
-    out = atomic_load_explicit(&q->out, memory_order_relaxed);
-    in = atomic_load_explicit(&q->in, memory_order_relaxed);
-    if (out == in)
-        return -1;
-    bool used = atomic_load_explicit(&q->isUsed[out & q->mask], memory_order_relaxed);
-    __VERIFIER_assume(used);
-    __VERIFIER_assume(atomic_compare_exchange_weak_explicit(&(q->out), &out, out + 1, memory_order_relaxed, memory_order_relaxed));
+    int cnt = 0;
+    do 
+    {
+        __VERIFIER_assume(++cnt < MAX_ITER);
+        out = atomic_load_explicit(&q->out, memory_order_relaxed);
+        in = atomic_load_explicit(&q->in, memory_order_relaxed);
+        if (out == in)
+            return false;
 
-    unsigned int r = atomic_load_explicit(&q->buffer[out &= q->mask], memory_order_relaxed);
+        used = atomic_load_explicit(&q->isUsed[out & q->mask], memory_order_acquire);
+    } while (!used || !atomic_compare_exchange_weak_explicit(&(q->out), &out, out + 1, memory_order_relaxed, memory_order_relaxed));
+
+    *retVal = q->buffer[out &= q->mask];
     atomic_store_explicit(&q->isUsed[out], false, memory_order_relaxed);
-    return r;
+    return true;
+}
+
+bool q_dequeue(queue_t *q, int **retVal) {
+    return dequeue(q, retVal);
 }
 
 int __thread tid;

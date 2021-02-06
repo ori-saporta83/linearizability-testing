b1f74fc492fc3de626abaf15cebc2a660dda3d3c
Ori Saporta <ori@vfunction.com>
2021-02-06 22:58:31 +0200

    small simplification for chase-lev

--- wrappers/chase-lev-wrapper.h
+++ wrappers/chase-lev-wrapper.h
@@ -2,37 +2,35 @@
 #include <stdint.h>
 #include <stdatomic.h>
 
-#ifndef LEN
-# define LEN 10
-#endif
+#define MAX_SIZE 10
 
 typedef int val_t;
 
 struct Deque {
 	atomic_uint_fast64_t bottom;
 	atomic_uint_fast64_t top;
-	val_t * _Atomic buffer[LEN]; // in fact, it should be marked as atomic
+	val_t * _Atomic buffer[MAX_SIZE]; // in fact, it should be marked as atomic
 	//due to the race between push and
 	// steal.
 };
 
-int64_t try_push(struct Deque *deq, int64_t N, val_t * data)
+int64_t try_push(struct Deque *deq, val_t * data)
 {
 	uint64_t b = atomic_load_explicit(&deq->bottom, memory_order_acquire);
 	uint64_t t = atomic_load_explicit(&deq->top, memory_order_relaxed);
 
 	int64_t len = (int64_t) (b - t);
-	if (len >= N) {
+	if (len >= MAX_SIZE) {
 		return -1; // full
 	}
 
-	atomic_store_explicit(&deq->buffer[b % N], data, memory_order_release);
+	atomic_store_explicit(&deq->buffer[b % MAX_SIZE], data, memory_order_release);
 
 	atomic_store_explicit(&deq->bottom, b + 1, memory_order_release);
 	return 0;
 }
 
-int64_t try_steal(struct Deque *deq, int64_t N, val_t **data)
+int64_t try_steal(struct Deque *deq, val_t **data)
 {
 	uint64_t t = atomic_load_explicit(&deq->top, memory_order_relaxed);
 
@@ -43,7 +41,7 @@ int64_t try_steal(struct Deque *deq, int64_t N, val_t **data)
 		return -1; // empty
 	}
 
-	*data = atomic_load_explicit(&deq->buffer[t % N], memory_order_acquire);
+	*data = atomic_load_explicit(&deq->buffer[t % MAX_SIZE], memory_order_acquire);
 
 	bool is_successful = atomic_compare_exchange_strong_explicit(&deq->top, &t, t + 1,
 							    memory_order_release,
@@ -52,17 +50,16 @@ int64_t try_steal(struct Deque *deq, int64_t N, val_t **data)
 }
 
 typedef struct Deque queue_t;
-int N = 10;
 
 void q_enqueue(queue_t *q, val_t *val) 
 {
-	int res = try_push(q, N, val);
+	int res = try_push(q, val);
     assert(res == 0);
 }
 
 bool q_dequeue(queue_t *q, val_t **retVal)
 {
-    int res = try_steal(q, N, retVal);
+    int res = try_steal(q, retVal);
     return res == 0;
 }
 

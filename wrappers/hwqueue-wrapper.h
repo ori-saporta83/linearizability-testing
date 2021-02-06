33c9edcb1176eefadfda79e930a37f35c9d915f6
Ori Saporta <ori@vfunction.com>
2021-02-06 23:22:09 +0200

    HW optimization attempt 2

--- wrappers/hwqueue-wrapper.h
+++ wrappers/hwqueue-wrapper.h
@@ -16,7 +16,7 @@ void q_enqueue(queue_t *q, val_t * val)
 {
 	int k = atomic_fetch_add_explicit(&(q->back), 1, memory_order_release);
 	// int k = atomic_fetch_add_explicit(&(q->back), 1, memory_order_acq_rel);
-	atomic_store_explicit(&(q->AR[k]), val, memory_order_release);
+	atomic_store_explicit(&(q->AR[k]), val, memory_order_relaxed);
 }
 
 bool q_dequeue(queue_t *q, val_t **retVal)
@@ -25,16 +25,11 @@ bool q_dequeue(queue_t *q, val_t **retVal)
 	int lback = atomic_load_explicit(&(q->back), memory_order_acquire);
 	val_t * lan = NULL;
 
-	// for (lan = k = 0; lan == 0; ++k) {
-	// 	__VERIFIER_assume(k < lback);
-	// 	lan = atomic_exchange_explicit(&(q->AR[k]), 0, memory_order_acquire);
-	// }
+
 	if (lback > 0) 
 	{
 		for (int k = 0; lan == NULL && k < lback; ++k) {
-			// __VERIFIER_assume(k < lback);
-			// lan = atomic_exchange_explicit(&(q->AR[k]), 0, memory_order_acq_rel);
-			lan = atomic_exchange_explicit(&(q->AR[k]), 0, memory_order_acquire);
+			lan = atomic_exchange_explicit(&(q->AR[k]), 0, memory_order_relaxed);
 		}
 	}
     *retVal = lan;
